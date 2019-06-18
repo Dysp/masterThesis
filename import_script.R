acab690d1963fc7e67341292396694e81cf60e7a
Dysp <dysperen@gmail.com>
2019-06-18 16:29:28 +0200

    Todays work

--- import_script.R
+++ import_script.R
@@ -3,6 +3,8 @@ id <- "1SxL0kpm8xXyslRKradFv03R6Sbr-YUkx09jOVW8RF8E"
 google_data <- drive_get(id = id)
 sheets <- sheets_get(google_data)
 
+all_data <- read_sheet(sheets$spreadsheet_id, range="AllTogether!A1:AL99")
+
 high_cholesterol <- read_sheet(sheets$spreadsheet_id, range="+CHOL, -Statin!A2:AL39")
 myalgia <- read_sheet(sheets$spreadsheet_id, range="+Myalgi, +Statin!A2:AL20")
 non_myalgia <- read_sheet(sheets$spreadsheet_id, range="-Myalgi, +Statin!A2:AL29")
--- main.R
+++ main.R
@@ -8,8 +8,8 @@ import_question = readline(prompt="Reimport data from Google? Type 'yes'")
 if(toupper(import_question) == "YES") { source("import_script.R") }
 
 # WIP: Learning with R for Data Science
-source("learning_r_chapter_3.R")
-source("learning_r_chapter_5.R")
+#source("learning_r_chapter_3.R")
+#source("learning_r_chapter_5.R")
 
 # WIP: Create normal distribution plots for
 source("create_normaldistribution_plots.R")
@@ -22,6 +22,47 @@ source("create_boxplots.R")
 ########### Below this line is just messing around ########### 
 
 # Find means for all variables for all groups with NA removed
+
+ggplot(data = all_data) + 
+  stat_summary(
+    mapping = aes(x = Label, y = `Total Cholesterol mmol/L`, color = Label),
+    fun.ymin = min,
+    fun.ymax = max,
+    fun.y = median) +
+  geom_rug(mapping = aes(y = `Total Cholesterol mmol/L`, color = Label)) + 
+  coord_flip()
+
+?geom_rug
+(new_all_data <- mutate(all_data, fitness_pr_kg = `ml/min`/ all_data$Weight))
+ggplot(data = new_all_data) + geom_boxplot(mapping = aes(y = fitness_pr_kg, color = Label))
+
+
+(grouped_by_label <- group_by(all_data, Label))
+(chol_summary <- summarise(grouped_by_label, 
+                          mean_chol = mean(`Total Cholesterol mmol/L`, na.rm = TRUE),
+                          sd_chol = sd(`Total Cholesterol mmol/L`, na.rm = TRUE),
+                          n_chol = n(),
+                          SE_chol = sd(`Total Cholesterol mmol/L`, na.rm = TRUE)/sqrt(n())))
+
+ggplot(data = chol_summary, mapping = aes(x = Label, y = mean_chol, fill = Label)) +
+  geom_bar(stat ="identity") +
+  geom_errorbar(aes(ymin = mean_chol - SE_chol, ymax = mean_chol + SE_chol), width=0.05)
+  
+?ylim
+
+(weight_summary <- summarise(grouped_by_label, 
+                             mean_weight = mean(Weight, na.rm = TRUE),
+                             sd_weight = sd(Weight, na.rm = TRUE),
+                             n_weight = n(),
+                             SE_weight = sd(Weight, na.rm = TRUE)/sqrt(n())))
+
+weight_plot <- ggplot(data = weight_summary, mapping = aes(x = Label, y = mean_weight, fill = Label)) +
+  geom_bar(stat ="identity") +
+  geom_errorbar(aes(ymin = mean_weight - sd_weight, ymax = mean_weight + sd_weight), width=0.01, color = "grey") +
+  geom_errorbar(aes(ymin = mean_weight - SE_weight, ymax = mean_weight + SE_weight), width=0.05)
+
+  ?geom_bar
+
 sapply(na.omit(control), mean)
 sapply(na.omit(myalgia), mean)
 sapply(na.omit(high_cholesterol), mean)
